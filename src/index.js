b3e501ca935aa7755089637a6ed450cb4587e9d4
Sam <sammintz1993@gmail.com>
2018-06-23 09:19:43 -0500

    moved book-list component to containers folder

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-// Create a new component 
-// This component should produce some HTML 
 
-const App = function(){
-	//jsx
-	return <div>Hi</div>
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
+import App from './components/app';
+import reducers from './reducers';
 
-// Take this component's HTML and put it on the page (in the dom)
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
-function() {
-	return [
-		{title: 'Javascript: The Good Parts' },
-		{title: 'Harry Potter'},
-		{title: 'The Dark Tower'},
-		{title: 'Eloquent Ruby'}
-	]
-}
+import { combineReducers } from 'redux';
+import BooksReducer from './reducer_books';
+
+const rootReducer = combineReducers({
+// key and value
+// books is state and value is whatever gets returned from BooksReducer function 
+  books: BooksReducer
+});
+
+export default rootReducer;
