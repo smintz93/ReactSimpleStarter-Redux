b7fa442473c89335dfa48754491b5d4833ce7971
Sam <sammintz1993@gmail.com>
2018-06-26 15:52:04 -0500

    book detail rendering on page

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
+import ActiveBook from './reducer_active_book';
 
 const rootReducer = combineReducers({
 // key and value
 // books is state and value is whatever gets returned from BooksReducer function 
-  books: BooksReducer
+  books: BooksReducer,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,6 +1,12 @@
 // state argument is not app state, only state 
 // this reducer is responsible for 
-export default function(state, action){
+	// if its undefined set state to null 
+export default function(state = null, action){
+	switch(action.type) {
+	case 'BOOK_SELECTED':
+		return action.payload;
+
+	}
 
 	return state 
 }
